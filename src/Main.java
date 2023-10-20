d6953aac7e754e5929c7fe1cd7be3ce961751490
Alicja <alicjakuszpit@wp.pl>
2023-10-20 21:29:40 +0200

    u1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world22!");
-        System.out.println("ok");
+        System.out.println("Hello world2lll2!");
     }
 }
